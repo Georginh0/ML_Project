ee7fa5ae65b5fa48a081c0fd8b47adbff08924cf
Georginh0 <george.sam@live.co.uk>
2024-09-17 23:59:10 +0100

    setup.py and requirements

---
+++ Src/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements.
+    """
+    if not os.path.exists(file_path):
+        return []  # Return an empty list if the file doesn't exist
+
+    with open(file_path, "r") as file_obj:
+        requirements = [line.strip() for line in file_obj if line.strip()]
+
+    # Remove '-e .' if it's present in the list
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+# Fetch requirements
+install_requires = get_requirements(requirements_path)
+
+setup(
+    name="MLproject",
+    version="0.0.1",
+    author="George Dogo",
+    author_email="George.sam@live.co.uk",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
